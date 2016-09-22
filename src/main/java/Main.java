3e8c397aea57316644d616723cb7fb19d08c3629
MasterFenrir <masterfenrir@outlook.com>
2016-09-22 11:51:05 +0200

    ADD: Basic setup for the application. Reads commandline input.

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Sander on 22-9-2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Controller controller = new Controller(args);
+            controller.run();
+        } catch (Exception e) {
+            System.out.println("Application quit due to: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
